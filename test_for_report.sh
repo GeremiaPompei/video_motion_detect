a5a0b3357380bce6829a47a24988da6117c578d2
Geremia Pompei <geremiapompei@gmail.com>
2022-07-11 20:26:54 +0200

    update plots

--- test_for_report.sh
+++ test_for_report.sh
@@ -9,11 +9,11 @@ do
     echo "${spp}_PIPELINE"
     echo "1_GRAYSCALE;2_SMOOTHING;3_DETECT_DIFFERENCE;TOTAL_TIME;"
     ./build/main $videoPath $threshold SEQUENTIAL $spp 1 $formatter
-    for nw in 1 2 4 8 16 32
+    for ((nw=2;nw<=32;nw++))
     do
         ./build/main $videoPath $threshold PARALLEL $spp $nw $formatter
     done
-    for nw in 1 2 4 8 16 32
+    for ((nw=2;nw<=32;nw++))
     do
         ./build/main $videoPath $threshold FASTFLOW $spp $nw $formatter
     done
