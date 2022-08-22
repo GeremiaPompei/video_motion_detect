01cc4a9a49177c7f548f37fe5186ca7fe1209e17
Geremia Pompei <geremiapompei@gmail.com>
2022-08-22 18:09:32 +0200

    refactor

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ videoPath="./media/test_1m.mp4"
 threshold="0.8"
 formatter=""
 
-for ((nw=0;nw<=30;nw++))
+./build/main $videoPath $threshold SEQUENTIAL 1 $formatter
+
+for ((nw=1;nw<=30;nw++))
 do
     ./build/main $videoPath $threshold NATIVE_THREADS $nw $formatter
     ./build/main $videoPath $threshold FASTFLOW $nw $formatter
 done
-
-./build/main $videoPath $threshold SEQUENTIAL 1 $formatter
